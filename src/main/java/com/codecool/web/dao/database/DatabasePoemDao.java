9a0e576b36fd96399500702048b7d3734afba5c0
beszteri <lajka89@gmail.com>
2019-06-02 18:51:14 +0200

    dto error fixed + index js

--- src/main/java/com/codecool/web/dao/database/DatabasePoemDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabasePoemDao.java
@@ -17,6 +17,7 @@ public class DatabasePoemDao extends AbstractDao implements PoemDao {
     }
 
     private Poem fetchPoem(ResultSet resultSet) throws SQLException {
+        // adott resultsetből kinyeri az adott adatokat és belőle egy uj objektumot hoz létre
         int id = resultSet.getInt("id");
         int userId = resultSet.getInt("user_id");
         String title = resultSet.getString("title");
@@ -29,9 +30,13 @@ public class DatabasePoemDao extends AbstractDao implements PoemDao {
     public List<Poem> findAllByUserId(int userId) throws SQLException {
         List<Poem> poems = new ArrayList<>();
         String sql = "SELECT * FROM poems WHERE user_id = ?;";
+        //a createStatementtől eltérően itt külön mengy az sql és a lekérdezés paraméterek, biztonságosabb.
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            //a ? helyén megadjuk, hogy milyen típusú adatot szerepelhet
         preparedStatement.setInt(1, userId);
+        //a resultset táblázatszerűen tárolja a lekért adatokat.
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                //amig van adat a resultsetben, addig új poemeket hozunk létre és adjuk azokat hozzá a listához.
                 while (resultSet.next()){
                     poems.add(fetchPoem(resultSet));
                 }
